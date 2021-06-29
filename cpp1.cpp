82cfd9acb992817722a3afad3f2a50822cc145c5
keerthu230 <77925947+keerthu230@users.noreply.github.com>
2021-06-29 17:59:53 +0530

    Add files via upload

---
+++ cpp1.cpp
@@ -0,0 +1,12 @@
+//cpp tutorial 1 
+#include<iostream>
+
+int main()              // main method
+{
+    std::cout<<"Hi keerthana\n ---welcome----\n you to the world \n\"c++\" programming "<<std::endl;
+
+    // cout -> with " << " this symbol std:: is used with cout, cin, endl -> std namespace .
+    
+
+
+}
